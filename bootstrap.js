b10b912d2a63a114c1f452b07be825e21649c10e
Anthony Ennis <tonyennis@yahoo.com>
2022-12-05 16:50:34 +0000

    latest cod

--- bootstrap.js
+++ bootstrap.js
@@ -955,13 +955,14 @@ function bootStrap(jspFile, cdn) {
                 authenticationModule(ts)
                 primaryAccountMembersModule(ts)
 
-                $("head").append("<script src=" + root +  '/services/modules/stripe/connectaccount/accountstatus/service.js?v=' + ts + '></script>');
-                $("head").append("<script src=" + root +  '/services/modules/stripe/functions.js?v=' + ts + '></script>');
-                $("head").append('<script src=' + root +  '/services/modules/business/uxWidgetSlideShow.js?v=' + ts + '><\/script>');
-                $("head").append("<script src=" + root +  '/services/modules/stripe/customer/login/model.js?v=' + ts + '></script>');
-                $("head").append("<script src=" + root +  '/services/modules/stripe/customer/login/controller.js?v=' + ts + '></script>');
-                $("head").append("<script src=" + root +  '/services/modules/stripe/customer/login/onchange.js?v=' + ts + '></script>');
-                $("head").append("<script src=" + root +  '/services/modules/stripe/customer/login/init.js?v=' + ts + '></script>');
+
+                $("head").append("<script  type=\"application/javascript\" src=" + root +  '/services/modules/stripe/connectaccount/accountstatus/service.js?v=' + ts + '></script>');
+                $("head").append("<script  type=\"application/javascript\" src=" + root +  '/services/modules/stripe/functions.js?v=' + ts + '></script>');
+                $("head").append('<script  type=\"application/javascript\" src=' + root +  '/services/modules/business/uxWidgetSlideShow.js?v=' + ts + '><\/script>');
+                $("head").append("<script  type=\"application/javascript\" src=" + root +  '/services/modules/stripe/customer/login/model.js?v=' + ts + '></script>');
+                $("head").append("<script  type=\"application/javascript\" src=" + root +  '/services/modules/stripe/customer/login/controller.js?v=' + ts + '></script>');
+                $("head").append("<script  type=\"application/javascript\" src=" + root +  '/services/modules/stripe/customer/login/onchange.js?v=' + ts + '></script>');
+                $("head").append("<script  type=\"application/javascript\" src=" + root +  '/services/modules/stripe/customer/login/init.js?v=' + ts + '></script>');
 
 
             });
